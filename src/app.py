2bc02f71e37df33056625f6fbfe57a62c6f65fad
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2021-11-21 18:08:02 -0300

    Primeira versão do multirequest (API bypass)

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
     datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -10,11 +11,17 @@ import dash_bootstrap_components as dbc
 import api, layout
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = 'Projeto CATALAN'
+
+# Default: Actual Month Consumption
+date_format_str = '%Y-%m-%d %H:%M:%S'
+today = datetime.today()
+first_day_month = today.replace(day=1)
 
 params = {
     'device_id': '198762435721298897478401',
-    'dt_start': '2021-10-01 00:00:00',
-    'dt_end': '2021-10-30 00:00:00'
+    'dt_start': first_day_month.strftime(date_format_str),
+    'dt_end': today.strftime(date_format_str)
 }
 
 fig = api.get_consumption(params['device_id'], params['dt_start'], params['dt_end'])
